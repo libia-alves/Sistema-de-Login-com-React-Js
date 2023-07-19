652206d8ee392e973bace75e012ff315a75c0974
Libia-alves <libia.canhete@gmail.com>
2023-07-19 01:57:26 -0300

    first commit

---
+++ login-reactJs/src/styles/global.js
@@ -0,0 +1,23 @@
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+
+  
+  }
+
+  body {
+    width: 100vw;
+    height: 100vh;
+    background-color:  		#CC6699;
+    font-family: Arial, Helvetica, sans-serif
+  }
+`;
+
+
+
+export default GlobalStyle;
